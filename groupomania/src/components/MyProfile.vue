92c642611a4d8f075e7950a5413d893a581ca9e5
Antoine P <78738410+lHimiko@users.noreply.github.com>
2021-07-29 23:22:53 +0200

    Adding confirmPassword input
    Add check before submit if there's a password and if both matches

--- groupomania/src/components/MyProfile.vue
+++ groupomania/src/components/MyProfile.vue
@@ -62,6 +62,20 @@
                                             <span class="badge badge-danger" v-if="!$v.password.minLength">{{$v.password.$params.minLength.min}} caractères min !.</span>
                                         </div>
                                     </div>
+                                    <div class="form-group form-group-sm">
+                                        <div class="col mx-auto position-relative">
+                                            <label for="confirmPassword"></label>
+                                            <div class="d-flex justify-content-center input-group">
+                                                <span class="icon position-absolute input-group-addon">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="30" fill="currentColor" class="bi bi-file-lock2-fill" viewBox="0 0 16 16">
+                                                        <path d="M7 6a1 1 0 0 1 2 0v1H7V6z"/>
+                                                        <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-2 6v1.076c.54.166 1 .597 1 1.224v2.4c0 .816-.781 1.3-1.5 1.3h-3c-.719 0-1.5-.484-1.5-1.3V8.3c0-.627.46-1.058 1-1.224V6a2 2 0 1 1 4 0z"/>
+                                                    </svg>
+                                                </span>
+                                                <input id="confirmPassword" name="confirmPassword" type="password" class="col-7 col-lg-6 form-control form-control-sm" v-model.trim="$v.confirmPassword.$model" placeholder="Confirmation">
+                                            </div>
+                                        </div>
+                                    </div>
                                     <button v-b-toggle.newPassword class="btn btn-dark btn-sm mt-3" type="submit">Valider</button>
                                 </div>
                             </form>
@@ -79,6 +93,7 @@
             <b-alert v-if="errorInputs" show dismissible variant="danger">Pseudo ou Email déjà utilisé</b-alert>
             <b-alert v-if="confirmUpdate" show dismissible variant="success">Profil mis à jour</b-alert>
             <b-alert v-if="errorDelete" show dismissible variant="danger">Une erreur est survenue. Veuillez contacter un administrateur</b-alert>
+            <b-alert v-if="differentConfirmPassword" show dismissible variant="danger">Les mots de passe sont différents</b-alert>
         </div>
     </div>
 </template>
@@ -114,16 +129,20 @@ export default {
             user: [],
             token: localStorage.getItem('token'),
             userId: parseInt(localStorage.getItem('userId')),
-            submited: false,
+
             pseudo: "",
             email: "",
             password: "",
+            confirmPassword: "",
             image: "",
             isAdmin: "",
+
+            submited: false,
             isActive: false,
             errorInputs: false,
             confirmUpdate: false,
-            errorDelete: false
+            errorDelete: false,
+            differentConfirmPassword: false,
         }
     },
 
@@ -136,7 +155,9 @@ export default {
         },
         password: {
             minLength: minLength(6)
-        }
+        },
+        confirmPassword: {
+        },
     },
 
     methods:{
@@ -146,8 +167,18 @@ export default {
         },
 
         updateProfile() {
+
             this.$v.$touch();
-            const formData = new FormData();
+            // Checks if password input is filled
+            if(this.password !== null) {
+                // Checks if both matches
+                if(this.password !== this.confirmPassword) {
+                    // Throw error if not
+                    this.differentConfirmPassword = true
+                    this.$router.go()
+                // Send the request if OK
+                } else {
+                    const formData = new FormData();
 
             if (this.image) {
                     formData.append("image", this.image);
@@ -177,6 +208,44 @@ export default {
                 .catch(() => {
                     this.errorInputs = true
                 })
+                }
+
+            // If password input is not filled,
+            } else { // send the request with other inputs
+
+                const formData = new FormData();
+                // If there is an image uploaded :
+                if (this.image) {
+                        formData.append("image", this.image);
+                        formData.append("userId",this.userId);
+                        formData.append("pseudo", this.pseudo);
+                        formData.append("email", this.email);
+                        formData.append("password", this.password);
+                // if not :
+                } else {
+                        formData.append("userId", this.userId);
+                        formData.append("pseudo", this.pseudo);
+                        formData.append("email", this.email);
+                        formData.append("password", this.password);
+                    }
+
+                axios
+                    .put(`http://localhost:3000/api/users/${this.userId}`, formData, {
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                            "Authorization": 'Bearer ' + this.token
+                        }
+                    })
+                    .then(() => {
+                        localStorage.setItem('pseudo', this.pseudo)
+                        this.confirmUpdate = true
+                        this.$router.go()
+                    })
+                    .catch(() => {
+                        this.errorInputs = true
+                        this.$router.go()
+                    })
+            }
         },
 
         deleteUser() {
